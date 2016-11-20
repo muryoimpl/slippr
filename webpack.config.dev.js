daaf5441063f4e0b204c31df50272d5294876b37
muryoimpl <muryoimpl@gmail.com>
2016-11-20 15:32:03 +0900

    Standarize webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   target: 'electron',
@@ -8,24 +8,24 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname),
-    filename: 'app.bundle.js',
+    filename: 'app.bundle.js'
   },
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel', query: { presets: ['react', 'es2015', 'stage-3'] }, exclude: path.resolve(__dirname, 'node_modules') },
-    ],
+      { test: /\.jsx?$/, loader: 'babel', query: { presets: ['react', 'es2015', 'stage-3'] }, exclude: path.resolve(__dirname, 'node_modules') }
+    ]
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx']
   },
 
   devtool: 'inline-source-map',
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"',
-    }),
-  ],
-};
+      'process.env.NODE_ENV': '"development"'
+    })
+  ]
+}
