ecdc0d7c471f1227cdc89d4e5abdbb166166a3d1
muryoimpl <muryoimpl@gmail.com>
2016-11-22 22:55:38 +0900

    :shirt:

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
-const electron = require('electron');
+const electron = require('electron')
+const { dialog, ipcMain } = require('electron')
+const fs = require('fs')
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
-let mainWindow;
+let mainWindow
 
-function createWindow() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+function createWindow () {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // for debug
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
+
+  ipcMain.on('show-file-dialog', (event, arg) => {
+    const options = {
+      properties: ['openFile', 'showHiddenFiles'],
+      filters: [
+        { name: 'markdown', extensions: ['markdown', 'md', 'mkd', 'mkd', 'mkdn', 'mdown'] }
+      ]
+    }
+
+    dialog.showOpenDialog(mainWindow, options, (filenames) => {
+      if (filenames) {
+        console.log(filenames[0])
+        fs.readFile(filenames[0], 'utf8', (error, text) => {
+          if (error != null) {
+            console.log('error: ' + error)
+            return
+          }
+
+          event.sender.send('reply-file-dialog', { markdown: text, filename: filenames[0] })
+        })
+      }
+    })
+  })
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
