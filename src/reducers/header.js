a317509206ff58fefa708c54421fea1aa97d3f81
muryoimpl <muryoimpl@gmail.com>
2016-11-24 01:17:08 +0900

    Use Immutable.js

--- src/reducers/header.js
+++ src/reducers/header.js
@@ -1,13 +1,14 @@
+import Immutable from 'immutable'
 import * as Types from '../constants/header'
 
-const initialState = {
-  filename: 'Not selected'
-}
+const initialState = Immutable.Map({
+  filename: ''
+})
 
 export default function headers (state = initialState, action) {
   switch (action.type) {
     case Types.SET_FILE_NAME:
-      return Object.assign({}, state, { filename: action.filename })
+      return state.merge({filename: action.filename}).toJS()
     case Types.SHOW_DEFAULT:
     default:
       return state
--- src/reducers/home.js
+++ src/reducers/home.js
@@ -1,11 +1,12 @@
+import Immutable from 'immutable'
 import * as Types from '../constants/home'
 
-const initialState = {}
+const initialState = Immutable.Map({ markdown: '', html: '' })
 
 export default function homes (state = initialState, action) {
   switch (action.type) {
     case Types.SET_MARKDOWN_TEXT:
-      return Object.assign({}, state, { markdown: action.markdown })
+      return state.merge({ markdown: action.markdown }).toJS()
     case Types.SHOW_HOME:
     default:
       return state
