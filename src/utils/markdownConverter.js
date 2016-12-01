46e266f95a8f96abfc451cf49d68a1fe369ac0d4
muryoimpl <muryoimpl@gmail.com>
2016-12-01 23:23:50 +0900

    Enable highlight.js

--- src/utils/markdownConverter.js
+++ src/utils/markdownConverter.js
@@ -1,10 +1,20 @@
 import MarkdownIt from 'markdown-it'
 import mdEmoji from 'markdown-it-emoji'
+import hljs from 'highlight.js'
 
 export function renderHtml (markdown) {
+  // TODO: markdown に「--」 への対応を入れる
   const md = new MarkdownIt({
     html: true,
-    typographer: true
+    typographer: true,
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return `<pre class="hljs"><code>${hljs.highlight(lang, str, true).value}</code></pre>`
+        } catch (__) {}
+      }
+      return ''
+    }
   }).use(mdEmoji)
 
   return md.render(markdown || '')
