2c74a1d179ecea2362f93aee6373d4db8ab341fc
muryoimpl <muryoimpl@gmail.com>
2017-02-13 22:03:25 +0900

    Inset img tag when dropping image file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,6 +26,34 @@ class Home extends React.Component {
     ipcRenderer.send('full-screen')
   }
 
+  // TODO: drag&drop 中に移動した位置に挿入されないのでなんとかしたい
+  handleDropImage (e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    const file = e.dataTransfer.files[0]
+    if (file.type.match('image/')) {
+      let tag
+      if (e.currentTarget.selectionStart && e.currentTarget.selectionStart !== 0) {
+        tag = `\n---\n${this.imgHTMLTag(file.path, file.name)}`
+      } else {
+        tag = `${this.imgHTMLTag(file.path, file.name)}`
+      }
+
+      const posStart = e.currentTarget.selectionStart
+      const posEnd = e.currentTarget.selectionEnd
+
+      let text = e.currentTarget.value
+      e.currentTarget.value = `${text.substring(0, posStart)}\n${tag}\n${text.substring(posEnd, text.length)}`
+
+      this.handleTextaraChange(e)
+    }
+  }
+
+  imgHTMLTag (path, name) {
+    return `<img src="file://${path}" height="100%" width="100%" alt="${name}">\n`
+  }
+
   render () {
     const { markdown } = this.props
 
@@ -34,7 +62,15 @@ class Home extends React.Component {
         <div className="pane">
           <form className="p-editor__pane">
             <div className="form-group p-editor__pane">
-              <textarea className="form-control p-editor__textarea" rows="10" value={markdown} autoFocus="true" onChange={e => this.handleTextaraChange(e)}></textarea>
+              <textarea
+                id="markdown-textarea"
+                className="form-control p-editor__textarea"
+                rows="10"
+                value={markdown}
+                autoFocus="true"
+                onDrop={e => this.handleDropImage(e)}
+                onChange={e => this.handleTextaraChange(e)}
+              ></textarea>
             </div>
           </form>
         </div>
