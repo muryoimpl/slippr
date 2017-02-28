15c903b3f9c3d63b0498712f5eb5b9d2f6226991
muryoimpl <muryoimpl@gmail.com>
2017-02-28 23:01:19 +0900

    Remove UglifyJsPlugin in webpack.config.dev.js
    To compile speed up

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -36,12 +36,6 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
     })
   ]
 }
